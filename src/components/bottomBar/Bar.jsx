1c2415dd68bb0e3a45c9bb21fdcdcce986b838fe
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-10-10 19:35:06 +0500

    Add files via upload

--- src/components/bottomBar/Bar.jsx
+++ src/components/bottomBar/Bar.jsx
@@ -4,10 +4,10 @@ export default function Bar(){
         <>
         <div className={Style.container}>
             <div className={Style.main}>
-            <a href="\home"><img src="images/home.png" alt=""   className={Style.user} /></a>
+            <a href="home"><img src="images/home.png" alt=""   className={Style.user} /></a>
             <a href=""><img src="images/reels.png" alt="" /></a>
             <a href=""><img src="images/shop.png" alt="" /></a>
-            <a href="/profile"><img src="images/user (1).png" alt=""  className={Style.user}/></a>
+            <a href="profile"><img src="images/user (1).png" alt=""  className={Style.user}/></a>
             <a href=""><img src="images/setting.png" alt="" /></a>
             </div>
         </div>
