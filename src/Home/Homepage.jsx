c80c4d4933b935372d92e993cf74f42064733447
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-10-11 19:31:02 +0500

    Add files via upload

--- src/Home/Homepage.jsx
+++ src/Home/Homepage.jsx
@@ -20,21 +20,21 @@ export default function Homepage(){
         </div>
         </div>
         <div className={Style.StoryContainer}>
-            <Story img="images/profile-1.jpg" name="Anna may"/>
-            <Story img="images/profile-2.jpg" name="john sam"/>
-            <Story img="images/profile-3.jpg" name="Alex"/>
-            <Story img="images/profile-4.jpg" name="Alina mathew" />
-            <Story img="images/profile-5.jpg" name="unknown user"/>
-            <Story img="images/profile-6.jpg" name="unknown user"/>
+            <Story img="images/profile-1.webp" name="Anna may"/>
+            <Story img="images/profile-2.webp" name="john sam"/>
+            <Story img="images/profile-3.webp" name="Alex"/>
+            <Story img="images/profile-4.webp" name="Alina mathew" />
+            <Story img="images/profile-5.webp" name="unknown user"/>
+            <Story img="images/profile-6.webp" name="unknown user"/>
             
             
         </div>
         <div className={Style.posts}>
-            <Post profile="images/profile-1.jpg" name="Anna may" time="7 Hours ago" img="images/post-1.jpg" like="5k Likes" comment="2.4k comments" share=" 100 shares" caption="Embracing the beauty of nature, where every leaf tells a story."/>
-            <Post profile="images/profile-2.jpg" name="john sam" time="2 Hours ago" img="images/post-2.jpg" like="2k Likes" comment="1k comments" share=" 400 shares" caption="Silence is not an option—raise your voice for justice!" />
-            <Post profile="images/profile-5.jpg" name="Alex" time="11 Minute ago" img="images/post-3.jpg" like="2 Likes" comment="0 comments" share=" 0 shares" caption="Good vibes and great times 🎉✨" />
-            <Post profile="images/profile-3.jpg" name="Alina mathew" time="13 Minute ago" img="images/post-5.jpg" like="20 Likes" comment="4 comments" share=" 0 shares" caption="Just me, being unapologetically myself." />
-            <Post profile="images/profile-4.jpg" name="unknown user" time="14 Hours ago" img="images/post-4.jpg" like="24 Likes" comment="10 comments" share=" 6 shares" caption="Stand up, speak out, and never back down!" />
+            <Post profile="images/profile-1.webp" name="Anna may" time="7 Hours ago" img="images/post-1.webp" like="5k Likes" comment="2.4k comments" share=" 100 shares" caption="Embracing the beauty of nature, where every leaf tells a story."/>
+            <Post profile="images/profile-2.webp" name="john sam" time="2 Hours ago" img="images/post-2.webp" like="2k Likes" comment="1k comments" share=" 400 shares" caption="Silence is not an option—raise your voice for justice!" />
+            <Post profile="images/profile-5.webp" name="Alex" time="11 Minute ago" img="images/post-3.webp" like="2 Likes" comment="0 comments" share=" 0 shares" caption="Good vibes and great times 🎉✨" />
+            <Post profile="images/profile-3.webp" name="Alina mathew" time="13 Minute ago" img="images/post-5.webp" like="20 Likes" comment="4 comments" share=" 0 shares" caption="Just me, being unapologetically myself." />
+            <Post profile="images/profile-4.webp" name="unknown user" time="14 Hours ago" img="images/post-4.webp" like="24 Likes" comment="10 comments" share=" 6 shares" caption="Stand up, speak out, and never back down!" />
             
         </div>
     </div>
--- src/profile-page/Profile.jsx
+++ src/profile-page/Profile.jsx
@@ -7,10 +7,10 @@ export default function Profile(){
         <div className={Style.container}>
            <div className={Style.content}>
             <div className={Style.banner}>
-                <img src="images/banner.jpg" alt="" />
+                <img src="images/banner.webp" alt="" />
             </div>
             <div className={Style.Profileimg}>
-                <img src="images/profile-4.jpg" alt="" />
+                <img src="images/profile-4.webp" alt="" />
                 <div className={Style.name}>
                 <h1>Alina mathews</h1>
                 <p>photographer / videographer</p>
